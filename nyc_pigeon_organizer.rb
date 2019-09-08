e3d30f1cd4cf0158c965a5fc24ac65c40ab4b194
Kathy B <kathybuggs@gmail.com>
2019-09-08 19:51:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+revised_data = {}
+ data[:color].each do |color, names|
+  names.each do |name|
+   if !revised_data.has_key?(name)
+   revised_data[name] = {color: []}
+   end
+   revised_data[name][:color] << color.to_s
+    end
+    end
+
+  data[:gender].each do |gender, names|
+    names.each do |name|
+      revised_data[name][:gender] = [gender.to_s]
+      binding.pry
+    end end
+
+  data[:lives].each do |location, names|
+    names.each do |name|
+      revised_data[name][:lives] = location.to_s
+      end end
+
+
+
+  revised_data
+  end
